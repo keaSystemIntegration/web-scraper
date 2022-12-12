62bd16b97c17db94abbdaa473bd79186b189f082
Itai Gramse <itai0004@stud.kea.dk>
2022-12-12 19:05:58 +0100

    gest all products from price runner

--- main.py
+++ main.py
@@ -6,17 +6,16 @@ from scrapy.utils.project import get_project_settings
 from price_runner_spidy.price_runner_spidy.spiders.category_spider import CategorySpider
 from price_runner_spidy.price_runner_spidy.spiders.product_spider import PriceRunnerSpider
 
+configure_logging()
 settings = get_project_settings()
-configure_logging(settings)
+# configure_logging(settings)
 runner = CrawlerRunner(settings)
 
 @defer.inlineCallbacks
 def crawl():
     yield runner.crawl(CategorySpider)
-    print("$$$$$$$$$$$")
-    print(CategorySpider().result)
-    print("$$$$$$$$$$$")
     yield runner.crawl(PriceRunnerSpider, start_urls=CategorySpider().result)
+    # yield runner.crawl(PriceRunnerSpider, start_urls=CategorySpider().result)
     reactor.stop()
 
 
--- price_runner_spidy/price_runner_spidy/pipelines.py
+++ price_runner_spidy/price_runner_spidy/pipelines.py
@@ -1,3 +1,4 @@
+
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
@@ -5,13 +6,36 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import sqlite3
 
 
 class PriceRunnerSpidyPipeline:
+
+    def __int__(self):
+        self.connection = sqlite3.connect("products.db")
+        self.curser = self.connection.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.curser.execute("""CREATE TABLE IF NOT EXIST products (
+            id TEXT PRIMARY KEY NOT NULL, 
+            name TEXT NOT NULL, 
+            sub_title TEXT NOT NULL, 
+            description TEXT NOT NULL, 
+            category TEXT NOT NULL, 
+            sub_category TEXT NOT NULL, 
+            price REAL NOT NULL, 
+            link TEXT NOT NULL, 
+            overall_rank REAL NOT NULL
+             )
+        ;""")
+
     def process_item(self, item, spider):
         print("//////////////////////")
         print(item)
         print("//////////////////////")
-
+        # self.curser.execute("""INSERT OR IGNORE INTO products VALUES (
+        #     ?,?,?,?,?,?,?,?,?
+        # );
+        # """)
         return item
